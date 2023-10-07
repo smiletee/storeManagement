f677c2bd17733493b0fc10a8471a4f48998a5e89
smiletee <smile_2902252@hotmail.com>
2023-10-08 01:12:36 +0700

    new page

--- script/home.js
+++ script/home.js
@@ -2,7 +2,11 @@ async function oninitHonePage() {
     console.log('--homw page---')
     $("#includeHtml").load("wellcome.html");
 
- 
+}
 
-  }
+async function oncilckNavMenu(this_){
+    console.log('oncilckNavMenu => ', this_.id)
+    $("#includeHtml").load(this_.id);
+
+}
 
