374448266cafa5a3169db438cd111ccf52691fd5
smiletee <147237205+smiletee@users.noreply.github.com>
2023-10-08 00:32:58 +0700

    Update home.js

--- script/home.js
+++ script/home.js
@@ -1,6 +1,5 @@
 async function oninitHonePage() {
     console.log('--homw page---')
-     $scope.pageName = 'wellcome.html';
     var app = angular.module('myApp', []);
 
     app.controller('myCtrl', function ($scope) {
