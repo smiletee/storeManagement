88d9550b5503509952de321f8d72c76550e20bbb
smiletee <smile_2902252@hotmail.com>
2023-10-08 00:49:17 +0700

    home

--- script/home.js
+++ script/home.js
@@ -1,10 +1,2 @@
-async function oninitHonePage() {
-    console.log('--homw page---')
-    var app = angular.module('myApp', []);
 
-    app.controller('myCtrl', function ($scope) {
-        $scope.pageName = 'wellcome.html';
-    });
-
-}
 
